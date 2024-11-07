19a245c815a4c20dd648693b09333916a6125c79
Darnol <32634736+Darnol@users.noreply.github.com>
2024-11-07 15:03:10 +0100

    Exploring differnet routes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 mod network;
-use crate::network::network_core::{show_interfaces, scan_interfaces};
+use crate::network::network_core::{show_interfaces, scan_interfaces, ping_host, ping_host_syscmd};
+
+use std::process::Command;
+use std::time::Duration;
+use std::net::IpAddr;
+use ping::ping;
 
 fn main() {
-    show_interfaces();
-    let active_hosts = scan_interfaces();
-    println!("Active hosts: {:?}", active_hosts);
+    // show_interfaces();
+
+    // let active_hosts = scan_interfaces();
+    // println!("Active hosts: {:?}", active_hosts);
+
+    // Test ping
+    let ip: IpAddr = "192.168.0.13".parse().unwrap();
+    println!("Pinging host: {:?}", ip);
+
+    let timeout = Duration::from_secs(1);
+    println!("Timeout: {:?}", timeout);
+
+    ping_host_syscmd(ip);
+
+    // match ping(ip, Some(timeout), Some(166), Some(3), Some(5), None) {
+    //     Ok(duration) => println!("Ping response time: {:?}", duration),
+    //     Err(e) => println!("Ping failed: {}", e),
+    // }
+
+    // ping_host(ip, timeout);
+
 }
--- src/network/network_core.rs
+++ src/network/network_core.rs
@@ -1,7 +1,13 @@
 use pnet::datalink::{self, NetworkInterface, Channel};
-use pnet::packet::Packet;
 use pnet::packet::ipv4::Ipv4Packet;
+
 use std::collections::HashSet;
+use std::process::Command;
+use std::time::Duration;
+use std::net::IpAddr;
+
+use ping::ping;
+use rand::random;
 
 pub fn show_interfaces() -> () {
     println!("Showing interfaces");
@@ -17,6 +23,34 @@ pub fn show_interfaces() -> () {
     for interface in interfaces_no_loopback {
         // Show the interface
         println!("Interface: {:?}", interface);
+    }
+}
+
+pub fn ping_host(ip: IpAddr, timeout: Duration) -> () {
+    let ping_stream = ping(ip, Some(timeout), Some(166), Some(3), Some(5), Some(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23])).unwrap();
+    println!("Ping stream: {:?}", ping_stream);
+
+    // let ping_stream = ping(ip, None).expect("Failed to create ping");
+    // for message in ping_stream {
+    //     match message {
+    //         pinger::PingResult::Pong(duration, _) => {
+    //             println!("Duration: {:?}", duration)
+    //         }
+    //         _ => {} // Handle errors, log ping timeouts, etc.
+    //     } 
+    // }
+}
+
+pub fn ping_host_syscmd(ip: IpAddr) -> () {
+    let output = Command::new("ping")
+        .arg("8.8.8.8")
+        .output()
+        .expect("Failed to execute ping");
+
+    if output.status.success() {
+        println!("Ping output:\n{}", String::from_utf8_lossy(&output.stdout));
+    } else {
+        println!("Ping failed:\n{}", String::from_utf8_lossy(&output.stderr));
     }
 }
 
@@ -42,60 +76,25 @@ pub fn scan_interfaces() -> HashSet<String> {
                 println!("Channel type: Ethernet");
 
                 let mut packet_count = 0;
+                const max_packets: u32 = 10;
 
                 while let Ok(packet) = rx.next() {
                     packet_count += 1;
                     println!("Received packet number: {:?}", packet_count);
 
                     if let Some(ipv4_packet) = Ipv4Packet::new(packet) {
-                        let source_ip = ipv4_packet.get_source();
-                        println!("Received packet from: {:?}", source_ip);
+                        println!("From {} to {}", ipv4_packet.get_source(), ipv4_packet.get_destination());
                     }
 
-                    if packet_count >= 10 {
+                    if packet_count >= max_packets {
                         break;
                     }
                 }
-                
-                // while attempts < max_attempts {
-                //     println!("Attempt: {}", attempts);
-                //     if let Ok(packet) = rx.next() {
-                //         if let Some(ipv4_packet) = Ipv4Packet::new(packet) {
-                //             println!("Received packet: {:?}", ipv4_packet);
-                //             let source_ip = ipv4_packet.get_source();
-                //             active_hosts.insert(source_ip.to_string());
-                //         }
-                //     } else {
-                //         // No packet received; increment the attempt counter
-                //         attempts += 1;
-                //     }
-                // }
             }
             _ => {
                 eprintln!("Unsupported channel type");
             }
         }
-
-        // match datalink::channel(&interface, Default::default()) {
-        //     Ok(Channel::Ethernet(_, mut rx)) => {
-        //         while let Ok(packet) = rx.next() {
-
-        //             println!("Received packet: {:?}", packet);
-
-        //             if let Some(ipv4_packet) = Ipv4Packet::new(packet) {
-        //                 let source_ip = ipv4_packet.get_source();
-        //                 active_hosts.insert(source_ip.to_string());
-        //                 break;
-        //             }
-        //         }
-        //     }
-        //     Ok(_) => {
-        //         eprintln!("Unsupported channel type");
-        //     }
-        //     Err(e) => {
-        //         eprintln!("Failed to create datalink channel: {}", e);
-        //     }
-        // }    
     }
 
     active_hosts
